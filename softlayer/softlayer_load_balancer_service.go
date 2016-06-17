e1e948f2363422a5df05db6baf106448a473d6e2
Tim Mulhern <tim.mulhern.contractor@weather.com>
2016-06-17 14:57:02 -0400

    Remove "getLoadBalancerVirtualServer" method

--- softlayer/softlayer_load_balancer_service.go
+++ softlayer/softlayer_load_balancer_service.go
@@ -26,7 +26,6 @@ type SoftLayer_Load_Balancer_Service interface {
 	CreateLoadBalancerVirtualServer(lbId int, createOptions *SoftLayer_Load_Balancer_Service_Group_CreateOptions) (bool, error)
 
 	GetObject(id int) (datatypes.SoftLayer_Load_Balancer, error)
-	GetLoadBalancerVirtualServer(lbId int, vsId int) (datatypes.Softlayer_Load_Balancer_Virtual_Server, error)
 
 	DeleteObject(id int) (bool, error)
 
