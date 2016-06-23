c48f588c74e5d43b05602d2a2cf8c675ece127f9
Mike Rieth <riethm@us.ibm.com>
2016-06-23 18:05:02 -0400

    Implement createObject and forceDeleteObject APIs for softlayer_scale_group

--- services/softlayer_scale_group.go
+++ services/softlayer_scale_group.go
@@ -3,6 +3,12 @@ package services
 import (
 	"github.com/TheWeatherCompany/softlayer-go/data_types"
 	"github.com/TheWeatherCompany/softlayer-go/softlayer"
+	"encoding/json"
+	"fmt"
+	"bytes"
+	"github.com/TheWeatherCompany/softlayer-go/common"
+	"errors"
+	"log"
 )
 
 type softlayer_Scale_Group_Service struct {
@@ -20,9 +26,56 @@ func (slsgs *softlayer_Scale_Group_Service) GetName() string {
 }
 
 func (slsgs *softlayer_Scale_Group_Service) CreateObject(template data_types.SoftLayer_Scale_Group) (data_types.SoftLayer_Scale_Group, error) {
-	return data_types.SoftLayer_Scale_Group{}, nil
+	parameters := data_types.SoftLayer_Scale_Group_Parameters{
+		Parameters: []interface{}{
+			template,
+		},
+	}
+
+	requestBody, err := json.Marshal(parameters)
+	log.Printf("[INFO]  ***** request body: %s", requestBody)
+	if err != nil {
+		return data_types.SoftLayer_Scale_Group{}, err
+	}
+
+	data, errorCode, err := slsgs.client.GetHttpClient().DoRawHttpRequest(fmt.Sprintf("%s/createObject", slsgs.GetName()), "POST", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return data_types.SoftLayer_Scale_Group{}, err
+	}
+
+	if common.IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not SoftLayer_Scale_Group#createObject\nResponse from SoftLayer: %s\nHTTP error code: '%d'", data, errorCode)
+		return data_types.SoftLayer_Scale_Group{}, errors.New(errorMessage)
+	}
+
+	err = slsgs.client.GetHttpClient().CheckForHttpResponseErrors(data)
+	if err != nil {
+		return data_types.SoftLayer_Scale_Group{}, err
+	}
+
+	softLayer_Scale_Group := data_types.SoftLayer_Scale_Group{}
+	err = json.Unmarshal(data, &softLayer_Scale_Group)
+	if err != nil {
+		return data_types.SoftLayer_Scale_Group{}, err
+	}
+
+	return softLayer_Scale_Group, nil
 }
 
-func (slsgs *softlayer_Scale_Group_Service) DeleteObject(group int) (bool, error) {
-	return true, nil
+func (slsgs *softlayer_Scale_Group_Service) ForceDeleteObject(group int) (bool, error) {
+	response, errorCode, err := slsgs.client.GetHttpClient().DoRawHttpRequest(fmt.Sprintf("%s/%d/forceDeleteObject", slsgs.GetName(), group), "GET", new(bytes.Buffer))
+	if err != nil {
+		return false, err
+	}
+
+	if res := string(response[:]); res != "true" {
+		return false, fmt.Errorf("Failed to force delete scale group with id '%d', got '%s' as response from the API.", group, res)
+	}
+
+	if common.IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not SoftLayer_Scale_Group#forceDeleteObject, HTTP error code: '%d'", errorCode)
+		return false, errors.New(errorMessage)
+	}
+
+	return true, err
 }
