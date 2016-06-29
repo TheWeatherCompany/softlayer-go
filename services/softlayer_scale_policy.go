40779122fee52c46db5afbdf078c11072c62e28f
Minsik Lee <minsikl@gmail.com>
2016-06-29 03:09:45 -0400

    Added GetObject and EditObject for SoftLayer_Scale_Policy.

--- services/softlayer_scale_policy.go
+++ services/softlayer_scale_policy.go
@@ -65,6 +65,67 @@ func (slsps *softlayer_Scale_Policy_Service) CreateObject(template datatypes.Sof
 	return softLayer_Scale_Policy, nil
 }
 
+func (slsps *softlayer_Scale_Policy_Service) GetObject(policyId int) (datatypes.SoftLayer_Scale_Policy, error) {
+	objectMask := []string{
+		"cooldown",
+		"id",
+		"name",
+		"scaleActions",
+		"scaleGroupId",
+		"oneTimeTriggers",
+		"repeatingTriggers",
+		"resourceUseTriggers.watches",
+		"triggers",
+	}
+
+	response, errorCode, err := slsps.client.GetHttpClient().DoRawHttpRequestWithObjectMask(fmt.Sprintf("%s/%d/getObject.json", slsps.GetName(), policyId), objectMask, "GET", new(bytes.Buffer))
+	if err != nil {
+		return datatypes.SoftLayer_Scale_Policy{}, err
+	}
+
+	if common.IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not SoftLayer_User_Customer#getObject, HTTP error code: '%d'", errorCode)
+		return datatypes.SoftLayer_Scale_Policy{}, errors.New(errorMessage)
+	}
+
+	policy := datatypes.SoftLayer_Scale_Policy{}
+	err = json.Unmarshal(response, &policy)
+	if err != nil {
+		return datatypes.SoftLayer_Scale_Policy{}, err
+	}
+
+	return policy, nil
+}
+
+func (slsps *softlayer_Scale_Policy_Service) EditObject(policyId int, template datatypes.SoftLayer_Scale_Policy) (bool, error) {
+	parameters := datatypes.SoftLayer_Scale_Policy_Parameters{
+		Parameters: []datatypes.SoftLayer_Scale_Policy{
+			template,
+		},
+	}
+
+	requestBody, err := json.Marshal(parameters)
+	if err != nil {
+		return false, err
+	}
+
+	response, errorCode, err := slsps.client.GetHttpClient().DoRawHttpRequest(fmt.Sprintf("%s/%d/editObject.json", slsps.GetName(), policyId), "POST", bytes.NewBuffer(requestBody))
+	if err != nil {
+		return false, err
+	}
+
+	if res := string(response[:]); res != "true" {
+		return false, errors.New(fmt.Sprintf("Failed to edit scale policy with id: %d, got '%s' as response from the API.", policyId, res))
+	}
+
+	if common.IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not SoftLayer_Scale_Policy#editObject, HTTP error code: '%d'", errorCode)
+		return false, errors.New(errorMessage)
+	}
+
+	return true, err
+}
+
 func (slsgs *softlayer_Scale_Policy_Service) DeleteObject(policy int) (bool, error) {
 	response, errorCode, err := slsgs.client.GetHttpClient().DoRawHttpRequest(fmt.Sprintf("%s/%d/deleteObject", slsgs.GetName(), policy), "GET", new(bytes.Buffer))
 	if err != nil {
