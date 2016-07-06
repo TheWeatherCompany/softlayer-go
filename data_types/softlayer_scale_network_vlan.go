cc0756fc2807ed7496c31519c7bac7f35efcec06
Mike Rieth <riethm@us.ibm.com>
2016-07-06 14:05:58 -0400

    Fix typo in json tag

--- data_types/softlayer_scale_network_vlan.go
+++ data_types/softlayer_scale_network_vlan.go
@@ -2,6 +2,6 @@ package data_types
 
 type SoftLayer_Scale_Network_Vlan struct {
 	Id            int                     `json:"id,omitempty"`
-	NetworkVlan   *SoftLayer_Network_Vlan `json:"networkVlan,omitempty`
+	NetworkVlan   *SoftLayer_Network_Vlan `json:"networkVlan,omitempty"`
 	NetworkVlanId int                     `json:"networkVlanId,omitempty"`
 }
