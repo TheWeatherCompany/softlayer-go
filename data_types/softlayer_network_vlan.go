8937b3159d97c7eab160b6c232fce24c131be3d6
Mike Rieth <riethm@us.ibm.com>
2016-07-06 08:57:48 -0400

    Allow network_vlan attributes to be omitted if empty

--- data_types/softlayer_network_vlan.go
+++ data_types/softlayer_network_vlan.go
@@ -5,12 +5,12 @@ import (
 )
 
 type SoftLayer_Network_Vlan struct {
-	AccountId       int        `json:"accountId"`
-	Id              int        `json:"Id"`
+	AccountId       int        `json:"accountId,omitempty"`
+	Id              int        `json:"Id,omitempty"`
 	ModifyDate      *time.Time `json:"modifyDate,omitempty"`
-	Name            string     `json:"name"`
-	NetworkVrfId    int        `json:"networkVrfId"`
-	Note            string     `json:"note"`
-	PrimarySubnetId int        `json:"primarySubnetId"`
-	VlanNumber      int        `json:"vlanNumber"`
+	Name            string     `json:"name,omitempty"`
+	NetworkVrfId    int        `json:"networkVrfId,omitempty"`
+	Note            string     `json:"note,omitempty"`
+	PrimarySubnetId int        `json:"primarySubnetId,omitempty"`
+	VlanNumber      int        `json:"vlanNumber,omitempty"`
 }
