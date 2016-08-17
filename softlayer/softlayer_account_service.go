206fb796d2ab61e717123e47f57fb5903d3603c2
Minsik Lee <minsikl@gmail.com>
2016-08-17 13:52:24 -0400

    Resolved rebase conflict.

--- softlayer/softlayer_account_service.go
+++ softlayer/softlayer_account_service.go
@@ -26,6 +26,5 @@ type SoftLayer_Account_Service interface {
 	GetHardware() ([]datatypes.SoftLayer_Hardware, error)
 	GetApplicationDeliveryControllersWithFilter(filter string) ([]datatypes.SoftLayer_Network_Application_Delivery_Controller, error)
 	GetApplicationDeliveryControllerLoadBalancersWithFilterAndMask(filter string, mask string) ([]datatypes.SoftLayer_Load_Balancer, error)
-	GetNetworkVlans(objectMask []string, objectFilter string) ([]datatypes.SoftLayer_Network_Vlan, error)
 	GetSubnets(objectMask []string, objectFilter string) ([]datatypes.SoftLayer_Network_Subnet, error)
 }
